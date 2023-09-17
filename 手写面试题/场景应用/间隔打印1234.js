1bce573832058b3ece7f06c0e3489e4dec120b5d
李名河 <2219845592@qq.com>
2023-09-17 13:30:11 +0800

    add method

--- 手写面试题/场景应用/间隔打印1234.js
+++ 手写面试题/场景应用/间隔打印1234.js
@@ -15,21 +15,31 @@
 
 // 1 2 3 4
 // setTimeout第二个参数之后为回调参数
-for (var i = 0; i < 4; i++) {
-  setTimeout(
-    function (i) {
-      console.log(++i);
-    },
-    i * 1000,
-    i
-  );
-}
+// for (var i = 0; i < 4; i++) {
+//   setTimeout(
+//     function (i) {
+//       console.log(++i);
+//     },
+//     i * 1000,
+//     i
+//   );
+// }
 
 // 使用闭包实现
-for (var i = 0; i < 5; i++) {
-  (function (i) {
-    setTimeout(function () {
-      console.log(i);
-    }, i * 1000);
-  })(i);
-}
+// for (var i = 0; i < 5; i++) {
+//   (function (i) {
+//     setTimeout(function () {
+//       console.log(i);
+//     }, i * 1000);
+//   })(i);
+// }
+
+// 递归试试
+(function printNubmer(cur, tar) {
+  if (cur > tar) return;
+  console.log(cur);
+  if (cur === tar) return;
+  setTimeout(() => {
+    printNubmer(++cur, tar);
+  }, 500);
+})(1, 4);
