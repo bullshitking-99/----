5d7fd7da35a4f3a5fb88a5bf4683ac10539e5e8d
李名河 <2219845592@qq.com>
2023-09-18 19:34:19 +0800

    add

--- 手写面试题/数据处理/嵌套数组深度.js
+++ 手写面试题/数据处理/嵌套数组深度.js
@@ -14,3 +14,15 @@ function Depth(arr) {
 }
 
 console.log(Depth([[1], [[1, 2]], [[[[1, 2, 3]]]]])); //5
+
+function diveDepth(arr, res = 1) {
+  if (!(arr instanceof Array)) return 0;
+
+  arr.forEach((item) => {
+    if (diveDepth(item) + 1 > res) res = diveDepth(item) + 1;
+  });
+
+  return res;
+}
+
+console.log(diveDepth([[1], [[1, 2]], [[[[1, 2, [[[]]], 3]]]]])); //8
