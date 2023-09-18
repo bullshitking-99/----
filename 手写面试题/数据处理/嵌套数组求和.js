74b9337e902ffe530e165956151a7f1fcd073fba
李名河 <2219845592@qq.com>
2023-09-18 19:16:48 +0800

    add

--- 手写面试题/数据处理/嵌套数组求和.js
+++ 手写面试题/数据处理/嵌套数组求和.js
@@ -14,3 +14,15 @@ function Sum(arr) {
 }
 
 console.log(Sum([[1], [[1, 2]], [[[[1, 2, 3]]]]])); // 10
+
+function diveSum(arr, res = 0) {
+  if (!arr.length) return res;
+
+  arr.forEach((item) => {
+    res += typeof item === "number" ? item : diveSum(item);
+  });
+
+  return res;
+}
+
+console.log(diveSum([[], [1], [[1, 2]], [[[[1, 2, 3]]], [1, 2, 3]]])); // 16
